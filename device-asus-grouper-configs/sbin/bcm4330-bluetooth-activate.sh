85c8b7352b59ac738f59341b0e87514ba6a7d158
beidl <dev.beidl@gmail.com>
2014-08-02 10:45:20 +0000

    [grouper] adapt bcm4330 firmware script to new maguro one
    Change-Id: I578ec6592a1668afd908d353191c142f2d3664b8

--- device-asus-grouper-configs/sbin/bcm4330-bluetooth-activate.sh
+++ device-asus-grouper-configs/sbin/bcm4330-bluetooth-activate.sh
@@ -1,4 +1,22 @@
-#! /bin/sh
-rfkill unblock bluetooth
-brcm_patchram_plus --patchram /system/etc/firmware/bcm4330.hcd --no2bytes --scopcm=0,2,0,0,0,0,0,0,0,0 --enable_hci --enable_lpm --baudrate 3000000 --use_baudrate_for_download --tosleep=50000 "/dev/ttyHS2"
+#!/bin/sh
+PATCHRAM_ARGS="--patchram /system/etc/firmware/bcm4330.hcd \
+               --no2bytes \
+               --scopcm=0,2,0,0,0,0,0,0,0,0 \
+               --enable_hci \
+               --enable_lpm \
+               --baudrate 3000000 \
+               --use_baudrate_for_download \
+               --tosleep=50000"
+
+if [ -f /system/bin/rfkill ]; then
+    /system/bin/rfkill unblock bluetooth
+else
+    rfkill unblock bluetooth
+fi
+
+if [ -f /system/bin/brcm_patchram_plus ]; then
+    /system/bin/brcm_patchram_plus $PATCHRAM_ARGS --bd_addr $BT_ADDR /dev/ttyHS2
+else
+    brcm_patchram_plus $PATCHRAM_ARGS --bd_addr $BT_ADDR /dev/ttyHS2
+fi
 
