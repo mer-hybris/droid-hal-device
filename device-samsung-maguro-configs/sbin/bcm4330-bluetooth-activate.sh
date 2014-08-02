0ed2289503a91126af7038c857c6deb523132121
beidl <dev.beidl@gmail.com>
2014-08-02 11:31:40 +0000

    [maguro][bluetooth] Fix Androids bin path
    Change-Id: I2904a1befa937de783f6d9e9276152c3548d06d2

--- device-samsung-maguro-configs/sbin/bcm4330-bluetooth-activate.sh
+++ device-samsung-maguro-configs/sbin/bcm4330-bluetooth-activate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+DROID_BIN="/usr/libexec/droid-hybris/system/bin"
 PATCHRAM_ARGS="--patchram /system/vendor/firmware/bcm4330.hcd \
                 --no2bytes \
                 --i2s=1,1,0,1 \
@@ -10,14 +11,14 @@ BT_ADDR=`cat /factory/bluetooth/bt_addr`
 echo $BT_ADDR > /var/lib/bluetooth/board-address
 
 
-if [ -f /system/bin/rfkill ]; then
-    /system/bin/rfkill unblock bluetooth
+if [ -f $DROID_BIN/rfkill ]; then
+    $DROID_BIN/rfkill unblock bluetooth
 else
     rfkill unblock bluetooth
 fi
 
-if [ -f /system/bin/brcm_patchram_plus ]; then
-    /system/bin/brcm_patchram_plus $PATCHRAM_ARGS --bd_addr $BT_ADDR /dev/ttyO1
+if [ -f $DROID_BIN/brcm_patchram_plus ]; then
+    $DROID_BIN/brcm_patchram_plus $PATCHRAM_ARGS --bd_addr $BT_ADDR /dev/ttyO1
 else
     brcm_patchram_plus $PATCHRAM_ARGS --bd_addr $BT_ADDR /dev/ttyO1
 fi
