0b166c8d6cbfc014f7baa3ef6019d342e453d95b
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2014-10-28 22:41:23 +0100

    [all][init] Propagate NOTIFY_SOCKET down to droid-init-done.sh
    NOTIFY_SOCKET has changed from @/org/freedesktop/systemd1/notify to
    /run/systemd/notify between systemd 208 and 216, so a hard-coded value
    breaks droid-hal-init with certain systemd version that wants
    /run/systemd/notify.
    
    The clean way is to propagate NOTIFY_SOCKET from droid-hal-startup.sh
    down to droid-init-done.sh.

--- dsp/bin/droid-hal-startup.sh
+++ dsp/bin/droid-hal-startup.sh
@@ -3,5 +3,10 @@ cd /
 sh /usr/libexec/droid/android-permission-fixup.sh &> /dev/null
 touch /dev/.coldboot_done
 export LD_LIBRARY_PATH=/usr/libexec/droid-hybris/system/lib/:/vendor/lib:/system/lib
+
+# Save systemd notify socket name to let droid-init-done.sh pick it up later
+mkdir -p /run/droid-hal
+echo $NOTIFY_SOCKET > /run/droid-hal/notify-socket-name
+
 exec /sbin/droid-hal-init
 
