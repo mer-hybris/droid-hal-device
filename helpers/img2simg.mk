c19bc327f9964c3632543c3a12df4145e69e3ef2
Kalle Jokiniemi <kalle.jokiniemi@jolla.com>
2015-04-20 10:54:01 +0300

    [tools] Fix img2simg build
    The output_file.c source uses too smart macro magics for it's own good,
    which doesn't play well with C99. Disabled -std=c99 option for
    img2simg to make it build without errors. Same added for simg2img
    as it builds against same object files.
    
    Signed-off-by: Kalle Jokiniemi <kalle.jokiniemi@jolla.com>

--- helpers/img2simg.mk
+++ helpers/img2simg.mk
@@ -8,6 +8,9 @@ SRCS+= sparse_err.c
 SRCS+= sparse_read.c
 SRCS+= img2simg.c
 
+# Too smart macros don't work with c99, filter it out.
+CFLAGS := $(filter-out -std=c99,$(CFLAGS))
+
 CPPFLAGS+= -Iinclude
 
 OBJS=$(SRCS:.c=.o)
