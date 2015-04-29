327842b06b36a66c67027d28ca43f5f3596a8d44
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2015-04-29 21:24:15 +0100

    [helpers][build_packages] no need for qtsensors

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -76,7 +76,6 @@ buildmw "https://github.com/mer-hybris/pulseaudio-modules-droid.git" rpm/pulseau
 buildmw qt5-feedback-haptics-droid-vibrator || die
 buildmw qt5-qpa-hwcomposer-plugin || die
 buildmw "https://github.com/mer-hybris/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
-buildmw "https://github.com/mer-packages/qtsensors.git" || die
 buildmw "https://github.com/mer-packages/sensorfw.git" rpm/sensorfw-qt5-hybris.spec || die
 read -p '"Build HA Middleware Packages built". Press Enter to continue.'
 popd
