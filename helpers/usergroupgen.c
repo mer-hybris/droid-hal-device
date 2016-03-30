1dce54f9ee8bfa4d6bfa86fcd95fa5247d385173
Boleslaw Tokarski <boleslaw.tokarski@gmail.com>
2016-03-30 10:44:42 +0200

    [usergen] Do not remove and re-add already existing users.
    The logic is:
    1. Generate name-id pairs required by droid layer to droid.ids.
    2. On installation create all the required user and groups and note their
       names and ids to droid.ids.created.
    3. On update cross-check the two files and only add and remove the name-ids
       that have been changed.
    
    ad. 1. usergroupgen.c no longer creates shellscript output, instead it creates
    droid.ids which is a simple text file with name-space-id format, one per line.
    
    ad. 2. postinstall script writes down every name-id it managed to create. It
    does not write any user creation of which failed.
    
    ad. 3. it is not verified during user/group removal if the user/group was
    modified by the user or with a third-party script. If the entry disappeared
    from droid-hal, we just remove it.
    
    Special note: A conversion mechanism was added for upgrades from previous dhd
    releases in the preinstall script. All users that were added in the previous
    release are considered to have been created.
    
    Change-Id: I2a13462bed5297f3c4ef601fcd26194c59444a3f

--- helpers/usergroupgen.c
+++ helpers/usergroupgen.c
@@ -32,32 +32,20 @@
 
 int main(int argc, char *argv[])
 {
-    int add, i;
+    int i;
 
-    if (argc != 2 || (strcmp(argv[1], "remove") != 0 &&
-                      strcmp(argv[1], "add") != 0)) {
-        fprintf(stderr, "Usage: %s [add|remove]\n", argv[0]);
+    if (argc != 1 ) {
+        fprintf(stderr, "Usage: %s\n", argv[0]);
         return 1;
     }
 
-    add = (strcmp(argv[1], "add") == 0);
-
-    printf("#!/bin/sh\n");
     for (i = 0; i < android_id_count; i++) {
         if (android_ids[i].aid == 0) {
             /* Skip creating/removing the root user */
             continue;
-        } else if (add) {
-            /* Add groups before users */
-            printf("groupadd -g %i %s\n", android_ids[i].aid,
-                    android_ids[i].name);
-            printf("useradd -M -N -s /sbin/nologin -d / -u %i -g %i %s\n",
-                    android_ids[i].aid, android_ids[i].aid,
-                    android_ids[i].name);
         } else {
-            /* Remove groups after users */
-            printf("userdel -f %s\n", android_ids[i].name);
-            printf("groupdel %s\n", android_ids[i].name);
+            /* List user-group entries */
+            printf("%s %i\n", android_ids[i].name, android_ids[i].aid);
         }
     }
 
