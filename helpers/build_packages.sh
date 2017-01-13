5d3cdeb59ff58ae0709a7b7a89c48e25c40527d1
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2017-01-13 11:22:15 +0000

    [build] Hack in Android 6 friendly pulseaudio modules
    Until it gets merged upstream
    
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jollamobile.com>

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -152,7 +152,12 @@ buildmw libhybris || die
 sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install zypper -n rm mesa-llvmpipe
 buildmw "https://github.com/nemomobile/mce-plugin-libhybris.git" || die
 buildmw ngfd-plugin-droid-vibrator || die
+# hack until upstream is sane
+if (grep -q 'PLATFORM_VERSION := 6.' $ANDROID_ROOT/build/core/version_defaults.mk); then
+buildmw "https://github.com/sledges/pulseaudio-modules-droid.git" android6 rpm/pulseaudio-modules-droid.spec || die
+else
 buildmw "https://github.com/mer-hybris/pulseaudio-modules-droid.git" rpm/pulseaudio-modules-droid.spec || die
+fi
 buildmw qt5-feedback-haptics-droid-vibrator || die
 buildmw qt5-qpa-hwcomposer-plugin qt-5.2 || die
 buildmw "https://github.com/mer-hybris/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
