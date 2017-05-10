60eaca23a16319e4ccc1a67b50d5b95e8f8febd9
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2017-05-10 13:41:11 +0100

    [build] qtscenegraph-adaptation has a new home

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -140,7 +140,7 @@ buildmw "https://github.com/nemomobile/mce-plugin-libhybris.git" || die
 buildmw ngfd-plugin-droid-vibrator || die
 buildmw qt5-feedback-haptics-droid-vibrator || die
 buildmw qt5-qpa-hwcomposer-plugin || die
-buildmw "https://github.com/mer-hybris/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
+buildmw "https://git.merproject.org/mer-core/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
 buildmw "https://git.merproject.org/mer-core/sensorfw.git" rpm/sensorfw-qt5-hybris.spec || die
 buildmw geoclue-providers-hybris jb36857 || die
 # build kf5bluezqt-bluez4 if not yet provided by Sailfish OS itself
