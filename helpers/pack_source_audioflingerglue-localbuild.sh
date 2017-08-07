f389bc1cfca3c629b13b0f7686c981246ded3e6a
Lewis Rockliffe <r0kk3rz@gmail.com>
2017-08-07 22:06:47 +0100

    [helpers] fix audioflingerglue-localbuild

--- helpers/pack_source_audioflingerglue-localbuild.sh
+++ helpers/pack_source_audioflingerglue-localbuild.sh
@@ -1,4 +1,8 @@
-if [ ! -f ./out/target/product/${DEVICE}/system/lib/libaudioflingerglue.so ]; then
+if [ -f ./out/target/product/${DEVICE}/system/lib/libaudioflingerglue.so ]; then 
+    DROIDLIB=lib
+elif [ -f ./out/target/product/${DEVICE}/system/lib64/libaudioflingerglue.so ]; then
+    DROIDLIB=lib64
+else
     echo "Please build audioflingerglue as per HADK instructions"
     exit 1
 fi
@@ -8,14 +12,14 @@ fold=hybris/mw/$pkg
 rm -rf $fold
 mkdir $fold
 
-mkdir -p $fold/out/target/product/${DEVICE}/system/lib
+mkdir -p $fold/out/target/product/${DEVICE}/system/${DROIDLIB}
 mkdir -p $fold/out/target/product/${DEVICE}/system/bin
 mkdir -p $fold/external/audioflingerglue
 
 cp ./external/audioflingerglue/*.h $fold/external/audioflingerglue/
-cp ./external/audioflingerglue/hybris.c $fold/external/audioflingerglue/
+cp ./external/audioflingerglue/hybris.c.in $fold/external/audioflingerglue/
 # Remove audioflingerglue bits from out/ (otherwise it would cause a conflict within droid-hal-$DEVICE):
-mv ./out/target/product/${DEVICE}/system/lib/libaudioflingerglue.so $fold/out/target/product/${DEVICE}/system/lib/
+mv ./out/target/product/${DEVICE}/system/${DROIDLIB}/libaudioflingerglue.so $fold/out/target/product/${DEVICE}/system/${DROIDLIB}/
 mv ./out/target/product/${DEVICE}/system/bin/miniafservice $fold/out/target/product/${DEVICE}/system/bin/
 
 tar -cjvf $fold.tgz -C $(dirname $fold) $pkg
