a9a9a5763c460804db7620c5df56678246a70d7e
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2018-11-06 09:38:06 +0000

    Fix indentation

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -102,72 +102,72 @@ fi
 . ./rpm/dhd/helpers/util.sh
 
 if [ "$BUILDDHD" == "1" ]; then
-builddhd
+    builddhd
 fi
 if [ "$BUILDCONFIGS" == "1" ]; then
-if [ -n "$(grep '%define community_adaptation' $ANDROID_ROOT/hybris/droid-configs/rpm/droid-config-$DEVICE.spec)" ]; then
-    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper se -i community-adaptation > /dev/null
-    ret=$?
-    if [ $ret -eq 104 ]; then
-        BUILDALL=y
-        buildmw https://github.com/mer-hybris/community-adaptation.git rpm/community-adaptation-localbuild.spec || die
-        BUILDALL=n
-    elif [ $ret -ne 0 ]; then
-        die "Could not determine if community-adaptation package is available, exiting."
+    if [ -n "$(grep '%define community_adaptation' $ANDROID_ROOT/hybris/droid-configs/rpm/droid-config-$DEVICE.spec)" ]; then
+        sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper se -i community-adaptation > /dev/null
+        ret=$?
+        if [ $ret -eq 104 ]; then
+            BUILDALL=y
+            buildmw https://github.com/mer-hybris/community-adaptation.git rpm/community-adaptation-localbuild.spec || die
+            BUILDALL=n
+        elif [ $ret -ne 0 ]; then
+            die "Could not determine if community-adaptation package is available, exiting."
+        fi
     fi
-fi
-# avoid a SIGSEGV on exit of libhybris client
-sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R ls /system/build.prop > /dev/null
-ret=$?
-if [ $ret -ne 0 ]; then
-    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R bash -c "mkdir -p /system; echo ro.build.version.sdk=99 > /system/build.prop"
-fi
-buildconfigs
+    # avoid a SIGSEGV on exit of libhybris client
+    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R ls /system/build.prop > /dev/null
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R bash -c "mkdir -p /system; echo ro.build.version.sdk=99 > /system/build.prop"
+    fi
+    buildconfigs
 fi
 
 if [ "$BUILDMW" == "1" ]; then
-sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install ssu domain sales
-sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install ssu dr sdk
+    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install ssu domain sales
+    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install ssu dr sdk
 
-sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install zypper ref -f
-sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install zypper -n install $ALLOW_UNSIGNED_RPM droid-hal-$DEVICE-devel
+    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install zypper ref -f
+    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install zypper -n install $ALLOW_UNSIGNED_RPM droid-hal-$DEVICE-devel
 
-android_version_major=$(sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R cat /usr/lib/droid-devel/droid-headers/android-version.h |grep "#define.*ANDROID_VERSION_MAJOR" |sed -e "s/#define.*ANDROID_VERSION_MAJOR//g")
+    android_version_major=$(sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R cat /usr/lib/droid-devel/droid-headers/android-version.h |grep "#define.*ANDROID_VERSION_MAJOR" |sed -e "s/#define.*ANDROID_VERSION_MAJOR//g")
 
-pushd $ANDROID_ROOT/hybris/mw > /dev/null
+    pushd $ANDROID_ROOT/hybris/mw > /dev/null
 
-if [ "$BUILDMW_REPO" == "" ]; then
-buildmw libhybris || die
-buildmw "https://github.com/mer-hybris/pulseaudio-modules-droid.git" rpm/pulseaudio-modules-droid.spec || die
-buildmw "https://github.com/nemomobile/mce-plugin-libhybris.git" || die
-buildmw ngfd-plugin-droid-vibrator rpm/ngfd-plugin-native-vibrator.spec || die
-buildmw qt5-feedback-haptics-droid-vibrator rpm/qt5-feedback-haptics-native-vibrator.spec || die
-buildmw qt5-qpa-hwcomposer-plugin || die
-buildmw qt5-qpa-surfaceflinger-plugin || die
-buildmw "https://git.merproject.org/mer-core/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
-buildmw "https://git.merproject.org/mer-core/sensorfw.git" rpm/sensorfw-qt5-hybris.spec || die
-if [ $android_version_major -ge 8 ]; then
-buildmw geoclue-providers-hybris rpm/geoclue-providers-hybris-binder.spec || die
-else
-buildmw geoclue-providers-hybris rpm/geoclue-providers-hybris.spec || die
-fi
-# build kf5bluezqt-bluez4 if not yet provided by Sailfish OS itself
-sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper se kf5bluezqt-bluez4 > /dev/null
-ret=$?
-if [ $ret -eq 104 ]; then
-    buildmw "https://git.merproject.org/mer-core/kf5bluezqt.git" rpm/kf5bluezqt-bluez4.spec || die
-    # pull device's bluez4 configs correctly
-    sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper remove bluez-configs-mer
-fi
-else
-buildmw $BUILDMW_REPO $BUILDSPEC_FILE || die
-fi
-popd > /dev/null
+    if [ "$BUILDMW_REPO" == "" ]; then
+        buildmw libhybris || die
+        buildmw "https://github.com/mer-hybris/pulseaudio-modules-droid.git" rpm/pulseaudio-modules-droid.spec || die
+        buildmw "https://github.com/nemomobile/mce-plugin-libhybris.git" || die
+        buildmw ngfd-plugin-droid-vibrator rpm/ngfd-plugin-native-vibrator.spec || die
+        buildmw qt5-feedback-haptics-droid-vibrator rpm/qt5-feedback-haptics-native-vibrator.spec || die
+        buildmw qt5-qpa-hwcomposer-plugin || die
+        buildmw qt5-qpa-surfaceflinger-plugin || die
+        buildmw "https://git.merproject.org/mer-core/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
+        buildmw "https://git.merproject.org/mer-core/sensorfw.git" rpm/sensorfw-qt5-hybris.spec || die
+        if [ $android_version_major -ge 8 ]; then
+            buildmw geoclue-providers-hybris rpm/geoclue-providers-hybris-binder.spec || die
+        else
+            buildmw geoclue-providers-hybris rpm/geoclue-providers-hybris.spec || die
+        fi
+        # build kf5bluezqt-bluez4 if not yet provided by Sailfish OS itself
+        sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper se kf5bluezqt-bluez4 > /dev/null
+        ret=$?
+        if [ $ret -eq 104 ]; then
+            buildmw "https://git.merproject.org/mer-core/kf5bluezqt.git" rpm/kf5bluezqt-bluez4.spec || die
+            # pull device's bluez4 configs correctly
+            sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper remove bluez-configs-mer
+        fi
+    else
+        buildmw $BUILDMW_REPO $BUILDSPEC_FILE || die
+    fi
+    popd > /dev/null
 fi
 
 if [ "$BUILDVERSION" == "1" ]; then
-buildversion
-echo "----------------------DONE! Now proceed on creating the rootfs------------------"
+    buildversion
+    echo "----------------------DONE! Now proceed on creating the rootfs------------------"
 fi
 
 if [ "$BUILDPKG" == "1" ]; then
