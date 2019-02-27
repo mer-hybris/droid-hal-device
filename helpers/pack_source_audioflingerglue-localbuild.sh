8fb88acaca98e6ddd3ca9732de9d4c572695eba8
ix5 <ix5@users.noreply.github.com>
2019-02-27 15:38:37 +0100

    helpers: Try to use $HABUILD_DEVICE for droidmedia and audioflingerglue
    If no $HABUILD_DEVICE is defined, fall back to $DEVICE.
    
    Example:
    $HABUILD_DEVICE=kagura
    $DEVICE=f8331
    Android's build system will create
    $ANDROID_ROOT/out/target/product/kagura

--- helpers/pack_source_audioflingerglue-localbuild.sh
+++ helpers/pack_source_audioflingerglue-localbuild.sh
@@ -1,6 +1,8 @@
-if [ -f ./out/target/product/${DEVICE}/system/lib/libaudioflingerglue.so ]; then 
+OUT_DEVICE=${HABUILD_DEVICE:-$DEVICE}
+
+if [ -f ./out/target/product/${OUT_DEVICE}/system/lib/libaudioflingerglue.so ]; then
     DROIDLIB=lib
-elif [ -f ./out/target/product/${DEVICE}/system/lib64/libaudioflingerglue.so ]; then
+elif [ -f ./out/target/product/${OUT_DEVICE}/system/lib64/libaudioflingerglue.so ]; then
     DROIDLIB=lib64
 else
     echo "Please build audioflingerglue as per HADK instructions"
@@ -12,15 +14,15 @@ fold=hybris/mw/$pkg
 rm -rf $fold
 mkdir $fold
 
-mkdir -p $fold/out/target/product/${DEVICE}/system/${DROIDLIB}
-mkdir -p $fold/out/target/product/${DEVICE}/system/bin
+mkdir -p $fold/out/target/product/${OUT_DEVICE}/system/${DROIDLIB}
+mkdir -p $fold/out/target/product/${OUT_DEVICE}/system/bin
 mkdir -p $fold/external/audioflingerglue
 
 cp ./external/audioflingerglue/*.h $fold/external/audioflingerglue/
 cp ./external/audioflingerglue/hybris.c.in $fold/external/audioflingerglue/
-# Remove audioflingerglue bits from out/ (otherwise it would cause a conflict within droid-hal-$DEVICE):
-mv ./out/target/product/${DEVICE}/system/${DROIDLIB}/libaudioflingerglue.so $fold/out/target/product/${DEVICE}/system/${DROIDLIB}/
-mv ./out/target/product/${DEVICE}/system/bin/miniafservice $fold/out/target/product/${DEVICE}/system/bin/
+# Remove audioflingerglue bits from out/ (otherwise it would cause a conflict within droid-hal-$OUT_DEVICE):
+mv ./out/target/product/${OUT_DEVICE}/system/${DROIDLIB}/libaudioflingerglue.so $fold/out/target/product/${OUT_DEVICE}/system/${DROIDLIB}/
+mv ./out/target/product/${OUT_DEVICE}/system/bin/miniafservice $fold/out/target/product/${OUT_DEVICE}/system/bin/
 
 tar -cjvf $fold.tgz -C $(dirname $fold) $pkg
 
