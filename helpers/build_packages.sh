72b9a833b969033c3dfe489c07d2e73b13a5c656
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2020-06-12 10:12:19 +0000

    helpers: build more needed packages

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -244,6 +244,7 @@ if [ "$BUILDMW" = "1" ]; then
             buildmw -u "https://github.com/mer-hybris/bluebinder" || die
             buildmw -u "https://github.com/mer-hybris/ofono-ril-binder-plugin" || die
             buildmw -u "https://github.com/mer-hybris/libncicore.git" || die
+            buildmw -u "https://github.com/mer-hybris/libnciplugin.git" || die
             buildmw -u "https://github.com/mer-hybris/nfcd-binder-plugin" || die
         fi
         buildmw -u "https://github.com/mer-hybris/pulseaudio-modules-droid.git" \
