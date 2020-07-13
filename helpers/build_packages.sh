0b0307a1e904316621496a709e7fa754a8b914b5
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2020-07-13 21:34:44 +0200

    aarch64: OBS and local build fixes
    [aarch64] OBS and local build fixes. JB#50495
    
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jolla.com>

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -108,6 +108,12 @@ while true; do
     shift
 done
 
+if [ "$PORT_ARCH" = "aarch64" ]; then
+    _LIB=lib64
+else
+    _LIB=lib
+fi
+
 if [ $# -ne 0 ]; then
     echo "unknown option(s): $@"
     exit 1
@@ -173,7 +179,7 @@ if [ "$BUILDMW" = "1" ]; then
         sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R -msdk-install zypper -n install $ALLOW_UNSIGNED_RPM droid-hal-$HABUILD_DEVICE-devel
     fi
 
-    android_version_major=$(sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R cat /usr/lib/droid-devel/droid-headers/android-version.h |grep "#define.*ANDROID_VERSION_MAJOR" |sed -e "s/#define.*ANDROID_VERSION_MAJOR//g")
+    android_version_major=$(sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -R cat /usr/$_LIB/droid-devel/droid-headers/android-version.h 2>/dev/null |grep "#define.*ANDROID_VERSION_MAJOR" |sed -e "s/#define.*ANDROID_VERSION_MAJOR//g")
 
     pushd $ANDROID_ROOT/hybris/mw > /dev/null
 
